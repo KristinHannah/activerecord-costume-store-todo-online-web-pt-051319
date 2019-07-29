32e7fe432e954b4678a35b18b65694e1bedf4429
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-29 01:33:41 +0000

    Automatically backed up by Learn

--- db/migrate/004_update_haunted_house.rb
+++ db/migrate/004_update_haunted_house.rb
@@ -1,9 +1,9 @@
 class UpdateHauntedHouse < ActiveRecord::Migration[5.1]
+  
   def change 
     add_column :haunted_houses, :price, :integer 
     add_column :haunted_houses, :description, :string 
     change_column(:haunted_houses, :family_friendly, :boolean)
   end 
-end 
 
 end 
