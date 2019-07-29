39a14ec4b215c3c4808fa39267571c870bac29cb
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-29 01:19:39 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,12 +2,12 @@
 
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
     def change 
-    create_table :costumes do |t|
+    create_table :costume_stores do |t|
       t.string :name 
-      t.integer :price 
-      t.integer :size 
-      t.string :image_url 
-      t.timestamp :created_at 
+      t.string :location 
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.timestamp :still_in_business 
       t.timestamp :updated_at
   end 
 end 
