2918b3e4d6b27ff3f4203af331f14dfa3fc7fb43
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-29 01:24:47 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,6 +2,16 @@
 
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   
-  
+   def change 
+    create_table :haunted_houses do |t|
+      t.string :name 
+      t.string :location 
+      t.string :theme
+      t.integer :family_friendly 
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :long_description
+  end 
+end 
   
 end 
